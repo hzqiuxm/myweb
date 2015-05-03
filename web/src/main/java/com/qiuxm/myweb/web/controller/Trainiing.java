caca4115582c7ccf7b8df5d9be32bf28ac065890
home <home@163.com>
2015-05-03 16:36:34 +0800

    增加jedis与spring的集成，并使用配置方式注入jedis连接池的单例

---
+++ web/src/main/java/com/qiuxm/myweb/web/controller/Trainiing.java
@@ -0,0 +1,24 @@
+package com.qiuxm.myweb.web.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by hzqiuxm on 2015/5/3.
+ */
+
+@Controller
+@RequestMapping("peixun")
+public class Trainiing {
+    private  static Logger log = Logger.getLogger(Trainiing.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(ModelMap modelMap){
+
+        return  "peixun";
+    }
+
+}
---
+++ webapp/src/main/webapp/WEB-INF/views/peixun.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hzqiuxm
+  Date: 2015/5/3
+  Time: 15:55
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>培训登记页面</title>
+  <jsp:include flush="true" page="/base.jsp"/>
+
+</head>
+<body>
+
+</body>
+</html>
